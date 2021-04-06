9ef085b827b4ba88b323a23ccbbbe8e285f5fc6e
kimjihw <kimal846@naver.com>
2021-04-06 17:38:40 +0900

    d

--- src/main/java/com/mail/springboot/controller/MailController.java
+++ src/main/java/com/mail/springboot/controller/MailController.java
@@ -1,4 +1,24 @@
 package com.mail.springboot.controller;
 
+import com.mail.springboot.dto.MailDto;
+import com.mail.springboot.service.MailService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
 public class MailController {
+    private final MailService mailService;
+
+    @GetMapping("/mail")
+    public String dispmail(){
+        return "mail";
+    }
+
+    @PostMapping("/mail")
+    public void execMail(MailDto mailDto){
+        mailService.mailSend(mailDto);
+    }
 }
